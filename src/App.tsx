8ad77efd80ea869770633073a1650a8c4a513487
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2023-05-20 23:39:38 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './TheVegCultureLogo.jpg';
+import vegImage from "./TheVegWeAreClosed.png";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	React.useEffect(() => {
+		let mainImage = document.getElementById("main-image") as HTMLElement;
+		mainImage.classList.add("animator");
+	}, []);
+	return (
+	<div className="App">
+		<div className="banner">
+			FREE DELIVERY TO KITCHENER, WATERLOO, CAMBRIDGE & GUELPH
+		</div>
+		<header className="header">
+			<img src={logo} className="logo" alt="" />
+			<nav>
+				<button className="nav-link" onClick={clickedNavLink}>
+					HOME
+				</button>
+				<button className="nav-link" onClick={clickedNavLink}>
+					ORDER NOW
+				</button>
+				<button className="nav-link" onClick={clickedNavLink}>
+					MENU
+				</button>
+				<button className="nav-link" onClick={clickedNavLink}>
+					HOW IT WORKS
+				</button>
+				<button className="nav-link" onClick={clickedNavLink}>
+					FAQ
+				</button>
+				<button className="nav-link" onClick={clickedNavLink}>
+					CONTACT
+				</button>
+			</nav>
+		</header>
+		<div id="content">
+			<img id="main-image" src={vegImage} alt="" />
+			stuff here
+		</div>
+	</div>
+	);
+}
+
+
+// Event Handlers
+
+function clickedNavLink(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+	let contentDiv = document.getElementById("content") as HTMLDivElement;
+	contentDiv.innerText = e.currentTarget.textContent || "";
+	switch (e.currentTarget.textContent) {
+		case "HOME":
+			
+			break;
+	
+		default:
+			break;
+	}
 }
 
 export default App;
